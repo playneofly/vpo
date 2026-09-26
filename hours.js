05e7079a75bdcff4ad15a366925f6489187eb0a3
playneofly <alirezacxs89@gmail.com>
2026-09-26 20:05:49 +0330

    Delete hours.js

--- hours.js
+++
@@ -1,8 +0,0 @@
-import { hoursInfo } from '../../lib/support.js';
-import { assertGate } from '../../lib/gate.js';
-
-export async function onRequestGet({ env, request }) {
-  const blocked = await assertGate(env, request);
-  if (blocked) return blocked;
-  return Response.json({ ok: true, hours: hoursInfo() });
-}
